f55ab36b67da387561117de17b1664e7fc29dd40
romadd1193 <romadd1193@gmail.com>
2018-09-27 19:43:39 -0500

    commit

--- app/src/main/java/listshell/jirmproductions/com/listshell/MainActivity.java
+++ app/src/main/java/listshell/jirmproductions/com/listshell/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private Fragment currentFragment = null;
     private FragmentTransaction ft;
 
-    //josh was here
+    //josh and ross was here
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
