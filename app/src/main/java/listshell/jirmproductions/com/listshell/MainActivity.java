41913dc78bf660bccd11d03a83c40561b939b247
romadd1193 <romadd1193@gmail.com>
2018-09-27 19:29:59 -0500

    Initial commit

---
+++ app/src/main/java/listshell/jirmproductions/com/listshell/MainActivity.java
@@ -0,0 +1,75 @@
+package listshell.jirmproductions.com.listshell;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private Fragment currentFragment = null;
+    private FragmentTransaction ft;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    currentFragment = new frag1();
+                    ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.content, currentFragment);
+                    ft.commit();
+                    return true;
+                case R.id.navigation_dashboard:
+                    currentFragment = new frag2();
+                    ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.content, currentFragment);
+                    ft.commit();
+                    return true;
+                case R.id.navigation_notifications:
+                    currentFragment = new frag3();
+                    ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.content, currentFragment);
+                    ft.commit();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ft = getSupportFragmentManager().beginTransaction();
+        currentFragment = new frag1();
+        ft.replace(R.id.content, currentFragment);
+        ft.commit();
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+    public void switchToFragment1() {
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.navigation, new frag1()).commit();
+    }
+
+    public void switchToFragment2() {
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.navigation, new frag2()).commit();
+    }
+
+    public void switchToFragment3() {
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.navigation, new frag3()).commit();
+    }
+}
